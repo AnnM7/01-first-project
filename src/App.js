88343a681550998b5a84be616a4f3e1779fe4c29
AnnM7 <annam210399@yandex.ru>
2022-06-10 19:36:36 +0300

    Fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src='https://i.pinimg.com/originals/fa/99/69/fa99695d46c1e28ef267519904f8dcb5.png'></img>
+            </header>
+            <nav className='nav'>
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Massages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://ymcagbw.org/sites/default/files/2020-03/gray_wolf.png'></img>
+                </div>
+                <div>
+                    Ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
