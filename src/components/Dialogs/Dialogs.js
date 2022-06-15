92cc105fb371687e5b4d9f7653b71e45561fa843
AnnM7 <annam210399@yandex.ru>
2022-06-15 19:27:43 +0300

    fix after 20 lessons

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialog
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Anna
+                </div>
+                <div className={s.dialog}>
+                    Jeka
+                </div>
+                <div className={s.dialog}>
+                    Edward
+                </div>
+                <div className={s.dialog}>
+                    Nikita
+                </div>
+                <div className={s.dialog}>
+                    Natasha
+                </div>
+                <div className={s.dialog}>
+                    Roma
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     )
 }
